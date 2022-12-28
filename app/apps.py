2b10ecc9b4d27e115e9c974dfe3d9c4daad56c37
Your Name <you@example.com>
2022-12-28 14:19:35 +0530

    first commit

---
+++ app/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from app.views import first
+
+class AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app'
+    'app',
+    
